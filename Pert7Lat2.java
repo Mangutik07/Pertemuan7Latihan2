21eb3ec4b60cf82930cc840338f38e97bb1f6269
Mangutik07 <101343081+Mangutik07@users.noreply.github.com>
2022-04-19 14:22:23 +0800

    Add files via upload
    Pertemuan 7, Latihan 2

---
+++ Pert7Lat2.java
@@ -0,0 +1,18 @@
+public class Pert7Lat2 {
+    public static void main(String[] args) {
+        Buah B = new Buah();
+        Leci L = new Leci();
+
+        B.setNAMA("Leci");
+        B.setWARNA("Merah");
+        B.setHarga(30.000);
+
+        B.tampilInfo();
+
+
+        L.setNAMA("Leci A");
+        L.setWARNA("Merah");
+        L.setHarga(25.000);
+        L.tampilInfo();
+    }
+}
